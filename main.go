3273d38bce70cee35baadb7a69ad472720566d17
Stephen Paulger <stephen.paulger@newspeak.org.uk>
2018-08-15 16:05:03 +0100

    check for panic using go/types

--- main.go
+++ main.go
@@ -1,41 +1,64 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"go/ast"
+	"go/importer"
 	"go/parser"
 	"go/token"
+	"go/types"
+	"log"
 )
-
-func getFuncName(ce *ast.CallExpr) (string, error) {
-	switch fun := ce.Fun.(type) {
-	case *ast.SelectorExpr:
-		return fmt.Sprintf("%s.%s", fun.X, fun.Sel), nil
-	case *ast.Ident:
-		return fun.Name, nil
-	}
-	return "", errors.New("could not determine call expression's identifier")
-}
 
 func main() {
 	fset := token.NewFileSet()
 
-	pkgs, err := parser.ParseDir(fset, ".", nil, 0)
+	astPkgs, err := parser.ParseDir(fset, ".", nil, 0)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	for _, pkg := range pkgs {
-		for _, f := range pkg.Files {
+	if 10 == 3 {
+		panic("wat")
+	}
+
+	conf := types.Config{Importer: importer.Default()}
+
+	for _, astPkg := range astPkgs {
+		for _, f := range astPkg.Files {
 			ast.Inspect(f, func(n ast.Node) bool {
 				ce, ok := n.(*ast.CallExpr)
-				if ok {
-					if fn, err := getFuncName(ce); err == nil {
-						fmt.Printf("%s:\t%s\n", fset.Position(ce.Pos()), fn)
-					}
+				if !ok {
+					// node is not a function call, continue walking the AST.
+					return true
 				}
+
+				typesPkg, err := conf.Check("", fset, []*ast.File{f}, nil)
+				if err != nil {
+					log.Fatal(err) // type error
+				}
+
+				ident, ok := ce.Fun.(*ast.Ident)
+				if !ok {
+					// Only current checking for panic, which will cast to Ident
+					// otherwise continue walking the AST.
+					return true
+				}
+
+				pos := ce.Pos()
+				inner := typesPkg.Scope().Innermost(pos)
+
+				_, obj := inner.LookupParent(ident.Name, pos)
+
+				if obj == nil {
+					return true
+				}
+
+				if b, ok := obj.(*types.Builtin); ok && (b.Name() == "panic") {
+					fmt.Printf("%s\t use of %s\n", fset.Position(pos), b.Name())
+				}
+
 				return true
 			})
 		}
